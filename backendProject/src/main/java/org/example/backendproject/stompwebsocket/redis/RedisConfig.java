2f22e649a26e48c8448d8b224d93bc254f1b4b90
parkhg09 <parkhg09@gmail.com>
2025-06-19 20:23:48 +0900

    docker 설정, socket + stomp + radis

---
+++ backendProject/src/main/java/org/example/backendproject/stompwebsocket/redis/RedisConfig.java
@@ -0,0 +1,30 @@
+package org.example.backendproject.stompwebsocket.redis;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.listener.PatternTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
+
+@Configuration
+@RequiredArgsConstructor
+public class RedisConfig {
+
+
+    private final RedisSubscriber redisSubscriber;
+
+    @Bean
+    public RedisMessageListenerContainer redisContainer(RedisConnectionFactory redisConnectionFactory) {
+        RedisMessageListenerContainer Container = new RedisMessageListenerContainer();
+        Container.setConnectionFactory(redisConnectionFactory);
+
+        Container.addMessageListener(new MessageListenerAdapter(redisSubscriber),new PatternTopic("room.*"));
+        Container.addMessageListener(new MessageListenerAdapter(redisSubscriber), new PatternTopic("private.*")); //귓속말
+
+        return Container;
+
+    }
+}
